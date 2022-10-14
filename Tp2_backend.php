d013387abb43dd0662d8caee930aa7e1ee131f78
Gloria Laura Ledesma <110358086+solestre@users.noreply.github.com>
2022-10-14 19:43:28 -0300

    Add files via upload

---
+++ Tp2_backend.php
@@ -0,0 +1,41 @@
+!><!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></Tp2 backend>
+  </head>
+  <body>
+    <?php
+    $a= 8;
+    if ($a es positivo); {
+    echo "$a es positivo";
+  }
+    elseif ($a)
+     echo "$a es negativo";
+     else ($a)
+     echo "$a es igual";
+     ?>
+  <br>
+  <hr>
+  <?php
+  $a= 5;
+  $b= 1;
+  $c= 10;
+  if ($a > $b <$c) {
+  echo $a es mayor a $b & mayor $c)
+  }
+  elseif ($a es mayor a $b & mayor $c); {
+  echo $a es mayor a $b & mayor $c)
+  else ($a es igual a $b & igual $c))
+  echo
+ ?>
+
+
+ <?php
+ $numero1= 5
+ $numero2= 2
+ if ($numero1 + numero2);
+ echo ""
+  ?>
+  </body>
+</html>
